4530d93964338dc64669e69130ff5d21d4f783f2
Andrei@psdtraining.de <andrey.tolmachev@gmail.com>
2019-11-18 23:17:50 +0300

    Initial russian version

---
+++ src/main/java/com/atolmachev/Greeting.java
@@ -0,0 +1,4 @@
+package com.atolmachev;
+
+public class Greeting {
+}
---
+++ src/test/java/com/atolmachev/GreetingTest.java
@@ -0,0 +1,13 @@
+package com.atolmachev;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class GreetingTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
